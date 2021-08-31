573fe7e0f4c0108fd77137d962f236eb81478cdf
Jaazield4 <85451089+Jaazield4@users.noreply.github.com>
2021-08-31 15:49:55 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -24,3 +24,24 @@ function buildTable(data) {
       );
     });
   }
+
+// Create a function to handle wat to do after an input is given, such as filtering by date
+function handleClick() {
+    // Telling D3 not only to look for where date values are stored on the webpage, but to actrually grab that info and hold it in the "date" variable
+    let date = d3.select('#datetime').property('value');
+    // Set a default filter and save to a new variable, if no filter is set then all the data from the orignial table will show
+    let filteredData = tableData;
+    // if statement to show only the rows where the date is equal to the date filter we created above
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredDatat will just be the original tableData
+    buildTable(filteredData);
+};
+
+// 
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Call the buildTable function
+buildTable(tableData);
